d325a6a36ae20b940c31ec463d46bfc8d3e8ddf7
Vinod Kumar <vinodiamonline@gmail.com>
2024-07-12 19:12:41 +0530

    Update vin.py

--- dags/vin.py
+++ dags/vin.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.bash import bashoperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 def say_hello():
