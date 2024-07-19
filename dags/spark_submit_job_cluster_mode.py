643d7766d95cd3bdb407fedb62b2d3245219314d
Vinod Kumar <vinodiamonline@gmail.com>
2024-07-19 17:42:54 +0530

    Create spark_submit_job_cluster_mode.py

---
+++ dags/spark_submit_job_cluster_mode.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+dag = DAG(
+    'spark_submit_job_cluster_mode',
+    description='A simple DAG that reads parquet file and print',
+    schedule_interval='@once',
+    start_date=datetime(2022, 1, 1),
+)
+
+task = SparkSubmitOperator(
+ task_id='submit_job',
+ application='testsparksubmit.py',
+ deploy-mode='cluster',
+ num_executors='2',
+ conn_id='kind-spark',
+ dag = dag
+)
+
+task
