29e8b371d82a8a43a0ee9fbe44ead795dbe49a10
Vinod Kumar <vinodiamonline@gmail.com>
2024-07-19 14:51:57 +0530

    Update testparquet.py

--- dags/testparquet.py
+++ dags/testparquet.py
@@ -2,14 +2,18 @@ from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
 
-with DAG('spark_submit_job',
-         start_date=datetime(2022, 1, 1),
-         schedule_interval='@daily) as dag:
+dag = DAG(
+    'spark_submit_job',
+    description='A simple DAG that reads parquet file and print',
+    schedule_interval='@once',
+    start_date=datetime(2022, 1, 1)
+)
 
-    submit_job = SparkSubmitOperator(
-          task_id='submit_job',
-          application='opt/airflow/testsparksubmit.py',
-          conn_id='kind-spark'
-    )
+task = SparkSubmitOperator(
+ task_id='submit_job',
+ application='opt/airflow/testsparksubmit.py',
+ conn_id='kind-spark',
+ dag = dag
+)
 
-    submit_job
+task
