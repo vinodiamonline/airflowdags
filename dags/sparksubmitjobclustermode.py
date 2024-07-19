631d4a2ad72a48f0098b42d24e551de91f04b79f
Vinod Kumar <vinodiamonline@gmail.com>
2024-07-19 17:47:19 +0530

    Update sparksubmitjobclustermode.py

--- dags/sparksubmitjobclustermode.py
+++ dags/sparksubmitjobclustermode.py
@@ -3,7 +3,7 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 from datetime import datetime
 
 dag = DAG(
-    'spark_submit_job_cluster_mode',
+    'spark_submit_job_in_cluster_mode',
     description='A simple DAG that reads parquet file and print',
     schedule_interval='@once',
     start_date=datetime(2022, 1, 1),
