1a8ea1ff190ce0b7d589c52d14fc27a95b18fb5c
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-03 02:11:36 +0530

    Update etlspeechkubernatescluster.py

--- dags/etlspeechkubernatescluster.py
+++ dags/etlspeechkubernatescluster.py
@@ -14,7 +14,8 @@ task = SparkSubmitOperator(
  application='/opt/airflow/etlspeechtime-assembly-0.1.0-SNAPSHOT.jar',
  java_class='speech',
  application_args=['admin', 'password', 'http://host.docker.internal:9000', 's3a://warehouse/micrawdata1', 's3a://warehouse/tbl_engagement_speech_silver', '3456000'],
- conn_id='kind-spark-cluster-mode',
+ conn_id='kind-spark',
+ num_executers='2',
  queue = 'kubernetes',
  dag = dag
 )
