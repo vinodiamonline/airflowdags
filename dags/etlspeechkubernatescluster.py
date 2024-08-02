25cd1079c8455ff9254da3ceefbf6a7810e6da6e
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-03 02:26:17 +0530

    Update etlspeechkubernatescluster.py

--- dags/etlspeechkubernatescluster.py
+++ dags/etlspeechkubernatescluster.py
@@ -16,6 +16,13 @@ task = SparkSubmitOperator(
  application_args=['admin', 'password', 'http://host.docker.internal:9000', 's3a://warehouse/micrawdata1', 's3a://warehouse/tbl_engagement_speech_silver', '3456000'],
  conn_id='kind-spark',
  num_executors=4,
+ conf={
+    'spark.kubernetes.container.image': 'airflow-spark-delta-hadoop-scala:1.0.0',  # Required image for driver
+    'spark.kubernetes.driver.docker.image': 'airflow-spark-delta-hadoop-scala:1.0.0',  # Docker image for driver
+    'spark.kubernetes.executor.docker.image': 'airflow-spark-delta-hadoop-scala:1.0.0',  # Docker image for executors (if different)
+    'spark.kubernetes.namespace': 'airflow',  # Kubernetes namespace
+    'spark.kubernetes.authenticate.driver.serviceAccountName': 'airflow-service',  # Service account (if required)
+ },
  queue = 'kubernetes',
  dag = dag
 )
