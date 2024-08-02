0cf766e08c0198848ccccd9050182740e970a346
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-02 12:39:29 +0530

    Update etlspeech.py

--- dags/etlspeech.py
+++ dags/etlspeech.py
@@ -11,7 +11,7 @@ dag = DAG(
 
 task = SparkSubmitOperator(
  task_id='etlspeech',
- application='/opt/airflow/etlspeech-assembly-0.1.0-SNAPSHOT.jar',
+ application='local:///opt/airflow/etlspeech-assembly-0.1.0-SNAPSHOT.jar',
  application_args=['admin', 'password', 'http://host.docker.internal:9000', 's3a://warehouse/micrawdata1', 's3a://warehouse/tbl_engagement_speech_silver', '3456000'],
  conn_id='kind-spark',
  queue = 'kubernetes',
