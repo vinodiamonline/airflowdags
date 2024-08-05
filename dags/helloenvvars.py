a07676c75a58a1f46e35ec2e131af98251cfbc2f
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-05 14:25:49 +0530

    Update and rename helloenvvars to helloenvvars.py

--- dags/helloenvvars.py
+++ dags/helloenvvars.py
@@ -6,10 +6,10 @@ from airflow.operators.python_operator import PythonOperator
 def print_hello():
     print('Hello env vars' + AIRFLOW_VAR_MY_S3_BUCKET)
 
-dag = DAG('hello_world', description='Hello World DAG',
+dag = DAG('helloenvvars', description='Hello World DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_k8s', python_callable=print_hello, dag=dag)
+hello_operator = PythonOperator(task_id='helloenvvars', python_callable=print_hello, dag=dag)
 
 hello_operator
