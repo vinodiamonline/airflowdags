f8320cf607d494e851277fa294e5e87545510c48
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-12 11:00:31 +0530

    Update etlspeechtimedag.py

--- dags/etlspeechtimedag.py
+++ dags/etlspeechtimedag.py
@@ -16,6 +16,17 @@ task = SparkSubmitOperator(
  conn_id='kind-spark',
  verbose=True,
  packages='io.delta:delta-spark_2.12:3.1.0,org.apache.hadoop:hadoop-aws:3.3.1',
+ conf={
+            'spark.sql.extensions': 'io.delta.sql.DeltaSparkSessionExtension',
+            'spark.sql.catalog.spark_catalog': 'org.apache.spark.sql.delta.catalog.DeltaCatalog',
+            'spark.jars.packages': 'org.apache.hadoop:hadoop-aws:3.3.1',
+            'spark.spark.hadoop.fs.s3a.access.key': 'admin',
+            'spark.hadoop.fs.s3a.secret.key': 'password',
+            'spark.hadoop.fs.s3a.endpoint': 'http://host.docker.internal:9000',
+            'spark.hadoop.fs.s3a.path.style.access': 'true',
+            'spark.databricks.delta.retentionDurationCheck.enabled': 'false',
+            'spark.hadoop.fs.s3a.aws.credentials.provider': 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider'
+        },
  queue = 'kubernetes',
  dag = dag
 )
