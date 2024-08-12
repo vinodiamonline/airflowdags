5223185570334c44726b2b869035ba9f440e1935
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-12 13:06:56 +0530

    Update sparkkubedag.py

--- dags/sparkkubedag.py
+++ dags/sparkkubedag.py
@@ -23,18 +23,44 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    # Define the SparkKubernetesOperator
     spark_job = SparkKubernetesOperator(
         task_id='spark_kubernetes_task',
-        application_file='./dags/repo/dags/spark_job.json',  # Path to the Kubernetes application YAML file
-        namespace='default',  # Kubernetes namespace
-        cluster_manager='kubernetes',
-        # Specify additional Spark configurations as needed
-        conf={
-            'spark.executor.memory': '4g',
-            'spark.executor.cores': '2',
-            'spark.driver.memory': '4g',
+        application={
+            'apiVersion': 'spark.apache.org/v1beta2',
+            'kind': 'SparkApplication',
+            'metadata': {
+                'name': 'spark-kubernetes-inline-example',
+                'namespace': 'default',
+            },
+            'spec': {
+                'type': 'Scala',
+                'mode': 'cluster',
+                'image': 'spark:latest',
+                'mainClass': 'org.example.Main',
+                'sparkVersion': '3.2.0',
+                'restartPolicy': {
+                    'type': 'OnFailure',
+                },
+                'driver': {
+                    'cores': 1,
+                    'memory': '1g',
+                },
+                'executor': {
+                    'cores': 1,
+                    'instances': 2,
+                    'memory': '1g',
+                },
+                'applications': [
+                    {
+                        'name': 'example',
+                        'className': 'org.example.Main',
+                        'arguments': ['arg1', 'arg2'],
+                    },
+                ],
+            },
         },
+        namespace='default',
+        cluster_manager='kubernetes',
     )
 
 # Define the task sequence
