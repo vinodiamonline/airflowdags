d9b681758ed54b50c8ec597f80709a99c71d5986
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-12 13:14:13 +0530

    Update sparkkubedag.py

--- dags/sparkkubedag.py
+++ dags/sparkkubedag.py
@@ -25,9 +25,9 @@ with DAG(
 
     spark_job = SparkKubernetesOperator(
         task_id="spark_task",
-        image="gcr.io/spark-operator/spark-py:v3.1.1",  # OR custom image using that
-        code_path="local://path/to/spark/code.py",
-        application_file="spark_job.json",  # OR spark_job_template.json
+        image="etlspeechtime:1.0.0",
+        code_path="local://app/etlspeechtime.jar",
+        application_file="spark_job.json",
         dag=dag,
     )
 
