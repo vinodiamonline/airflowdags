831d585986cd4bf430a7864adddd4213d955c19d
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-12 13:33:53 +0530

    Update sparkkubedag.py

--- dags/sparkkubedag.py
+++ dags/sparkkubedag.py
@@ -9,7 +9,7 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    'retries': 1,
+    'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
