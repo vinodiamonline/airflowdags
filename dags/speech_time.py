287e0d2cdde905219a6ae41a5a02bd895e526641
Vinod Kumar <vinodiamonline@gmail.com>
2024-08-26 04:28:40 +0530

    Update speech_time.py

--- dags/speech_time.py
+++ dags/speech_time.py
@@ -45,7 +45,7 @@ with DAG(
         namespace='airflow',
         application_file='speech_time.yaml',
         kubernetes_conn_id='kind-spark-cluster',
-        arguments=[S3_ACCESS_KEY, "password", "http://host.docker.internal:9000", "s3a://warehouse/micrawdata3", "s3a://warehouse/tbl_engagement_speech_silver", "7776000"]
+        arguments=["admin", "password", "http://host.docker.internal:9000", "s3a://warehouse/micrawdata3", "s3a://warehouse/tbl_engagement_speech_silver", "7776000"]
 )
 
 # Define the task sequence
