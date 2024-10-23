3577e69e6b8e99d583904bae58d7159e27f37c5b
Vinod Kumar <vinodiamonline@gmail.com>
2024-10-23 13:26:10 +0530

    Create vacuum_delta_table.py

---
+++ dags/vacuum_delta_table.py
@@ -0,0 +1,43 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from pyspark.sql import SparkSession
+
+def vacuum_table():
+    print('Hello from spark!')
+
+    spark = SparkSession.builder \
+        .appName("vacuum_delta_table") \
+        .master("local[*]") \
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0") \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.1") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.hadoop.fs.s3a.access.key", "admin") \
+        .config("spark.hadoop.fs.s3a.secret.key", "password") \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000") \
+        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+        .getOrCreate()
+       
+    data = [
+        ("Alice", 34),
+        ("Bob", 45),
+        ("Cathy", 29),
+        ("David", 40)
+    ]
+    columns = ["Name", "Age"]
+    spark.createDataFrame(data, columns).show()
+
+
+dag = DAG('vacuum_delta_table', description='Vacuum delta table from DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2024, 1, 1), catchup=False)
+
+vacuum_table = PythonOperator(
+    task_id='vacuum_delta_table', 
+    python_callable=vacuum_table
+    dag=dag
+    )
+
+vacuum_table
