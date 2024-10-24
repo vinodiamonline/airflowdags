d0af99775c81157c80d96ce8f28286ebb60b387a
Vinod Kumar <vinodiamonline@gmail.com>
2024-10-24 11:33:43 +0530

    Update vacuum_delta_table.py

--- dags/vacuum_delta_table.py
+++ dags/vacuum_delta_table.py
@@ -1,45 +1,92 @@
+#
+# Airflow DAG to run vacumm over obsolete data
+#
+
 from airflow import DAG
-from datetime import datetime
+from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
 from pyspark.sql import SparkSession
-from delta.tables import *
+import logging
+import os
 
+# Define logging
+logger = logging.getLogger(__name__)
+delta_table_path = "s3a://warehouse/color_10/"
+retention_hours = 168
+retention_check = "True"
+
+# Vacuum table Method
 def vacuum_table():
-    print('Hello from spark!')
+    S3_ACCESS_KEY = os.getenv("AWS_S3_ACCESS_KEY"),
+    S3_SECRET_KEY = os.getenv("AWS_S3_SECRET_KEY"),
+    S3_END_POINT = "http://host.docker.internal:9000" # os.getenv("AWS_S3_END_POINT")
 
-    spark = SparkSession.builder \
-        .appName("k2d") \
-        .master("local[*]") \
-        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0,org.apache.hadoop:hadoop-aws:3.3.1") \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.hadoop.fs.s3a.access.key", "admin") \
-        .config("spark.hadoop.fs.s3a.secret.key", "password") \
-        .config("spark.hadoop.fs.s3a.endpoint", "http://host.docker.internal:9000") \
-        .config("spark.hadoop.fs.s3a.path.style.access", "true") \
-        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
-        .getOrCreate()
-
-    delta_table_path = "s3a://warehouse/color_10/"
-    
-    spark.sql(f"VACUUM delta.`s3a://warehouse/color_10/` RETAIN 168 HOURS")
-
-    spark.read.format("delta").load(delta_table_path).printSchema()
-
-    # Stop the Spark session
-    spark.stop()
-    
-    print('Done!!!')
+    if (len(S3_ACCESS_KEY) > 0) and (len(S3_SECRET_KEY) > 0) and (len(S3_END_POINT) > 0):
+        logger.info("Start vacuuming!!!")
 
 
-dag = DAG('vacuum_delta_table', description='Vacuum delta table from DAG',
-          schedule_interval=None, # '@hourly',  # Runs every hour
-          start_date=datetime(2024, 1, 1), catchup=False)
+        if(retention_hours < 168) :
+            retention_check = "False"
 
-vacuum_table = PythonOperator(
-    task_id='vacuum_delta_table', 
-    python_callable=vacuum_table,
-    dag=dag
+        spark = SparkSession.builder \
+            .appName("vacuum") \
+            .master("local[*]") \
+            .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0,org.apache.hadoop:hadoop-aws:3.3.1") \
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+            .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+            .config("spark.hadoop.fs.s3a.aws.credentials.provider",
+                    "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+            .config("spark.hadoop.fs.s3a.access.key", S3_ACCESS_KEY) \
+            .config("spark.hadoop.fs.s3a.secret.key", S3_SECRET_KEY) \
+            .config("spark.hadoop.fs.s3a.endpoint", S3_END_POINT) \
+            .config("spark.databricks.delta.retentionDurationCheck.enabled", retention_check) \
+            .getOrCreate()
+
+        spark.sql(f'VACUUM delta.`{delta_table_path}` RETAIN {retention_hours} HOURS')
+
+        # for testing
+        spark.read.format("delta").load(delta_table_path).printSchema()
+
+        # Stop the Spark session
+        spark.stop()
+
+        logger.info("Vacuum complete!!!")
+    else:
+        logger.info("Invalid params ",
+            len(S3_ACCESS_KEY)
+            + len(S3_SECRET_KEY)
+            + len(S3_END_POINT)
+        )
+
+# Vacuum table Method end
+
+# Define default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+# Define the DAG
+with DAG(
+    'vacuum_delta_table',
+    default_args=default_args,
+    description='A DAG to vacuum delta table',
+    schedule_interval=None,
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['etl']
+) as dag:
+    # Define Operator
+    vacuum_table = PythonOperator(
+        task_id='vacuum_delta_table',
+        namespace='airflow',
+        python_callable=vacuum_table,
+        dag=dag
     )
 
+# Define the task sequence
 vacuum_table
