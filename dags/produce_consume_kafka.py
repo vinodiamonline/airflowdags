c005a9b6280d0d966a23af1819384e7793d6bbf3
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-05 16:50:51 +0530

    Update produce_consume_kafka.py

--- dags/produce_consume_kafka.py
+++ dags/produce_consume_kafka.py
@@ -1,44 +1,16 @@
-from airflow import DAG
-from airflow.providers.apache.kafka.operators.produce import ProduceToTopicOperator
-from airflow.providers.apache.kafka.hooks.produce import KafkaProducerHook
 from datetime import datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 
-# Kafka connection ID defined in Airflow's connection settings
-KAFKA_CONN_ID = 'kafka_default'
 
-# The Kafka topic where the message will be sent
-KAFKA_TOPIC = 'example_topic'
+def print_hello():
+    print('Hello world from first Airflow DAG!')
 
-# The message payload to be sent to Kafka
-message_payload = 'Hello, Kafka! This is a message from Airflow.'
+dag = DAG('produce_consume_kafka', description='Hello World DAG',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2017, 3, 20), catchup=False)
 
-# Default arguments
-default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2024, 11, 5),
-    'retries': 0,
-}
+hello_operator = PythonOperator(task_id='hello_k8s', python_callable=print_hello, dag=dag)
 
-def create_messages_function(**context) :
-    print("hello world")
-    return self
-
-# Define the DAG
-with DAG(
-    'produce_consume_kafka',
-    default_args=default_args,
-    schedule_interval=None,  # This is triggered manually
-    catchup=False,
-    tags=['example', 'kafka'],
-) as dag:
-
-    # Task to produce a message to the Kafka topic
-    produce_task = ProduceToTopicOperator(
-        task_id='produce_to_topic',
-        topic='your_topic',
-        producer_function=create_messages_function,
-        producer_function_args=(b"test")
-    )
-
-    # Set task dependencies
-    produce_task
+hello_operator
