ab75626ee997619a57bfacf2619204a7b54fb2d8
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-05 16:13:55 +0530

    Update produce_consume_kafka.py

--- dags/produce_consume_kafka.py
+++ dags/produce_consume_kafka.py
@@ -1,104 +1,41 @@
-"""
-### DAG which produces to and consumes from a Kafka cluster
-
-This DAG will produce messages consisting of several elements to a Kafka cluster and consume
-them.
-"""
-
-from airflow.decorators import dag, task
-from pendulum import datetime
+from airflow import DAG
 from airflow.providers.apache.kafka.operators.produce import ProduceToTopicOperator
-from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
-import json
-import random
+from airflow.providers.apache.kafka.hooks.produce import KafkaProducerHook
+from datetime import datetime
 
-YOUR_NAME = "Vinod"
-YOUR_PET_NAME = "kutta"
-NUMBER_OF_TREATS = 5
-KAFKA_TOPIC = "kutta_topic"
+# Kafka connection ID defined in Airflow's connection settings
+KAFKA_CONN_ID = 'kafka_default'
 
+# The Kafka topic where the message will be sent
+KAFKA_TOPIC = 'example_topic'
 
-def prod_function(**context):
-    """Produces `num_treats` messages containing the pet's name, a randomly picked
-    pet mood post treat and whether or not it was the last treat in a series."""
+# The message payload to be sent to Kafka
+message_payload = 'Hello, Kafka! This is a message from Airflow.'
 
-    num_treats = 5
-    pet_name = "kutta"
-    for i in range(num_treats):
-        final_treat = False
-        pet_mood_post_treat = random.choices(
-            ["content", "happy", "zoomy", "bouncy"], weights=[2, 2, 1, 1], k=1
-        )[0]
-        if i + 1 == num_treats:
-            final_treat = True
-        yield (
-            json.dumps(i),
-            json.dumps(
-                {
-                    "pet_name": pet_name,
-                    "pet_mood_post_treat": pet_mood_post_treat,
-                    "final_treat": final_treat,
-                }
-            ),
-        )
+# Default arguments
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 11, 5),
+    'retries': 1,
+}
 
-
-def consume_function(**context):
-    "Takes in consumed messages and prints its contents to the logs."
-    message = "hello world"
-    print(
-        f"Message {message}!"
-    )
-
-
-@dag(
-    start_date=datetime(2023, 4, 1),
-    schedule=None,
+# Define the DAG
+with DAG(
+    'produce_consume_treats',
+    default_args=default_args,
+    schedule_interval=None,  # This is triggered manually
     catchup=False,
-    render_template_as_native_obj=True,
-)
-def produce_consume_treats():
-    @task
-    def get_your_pet_name(pet_name=None):
-        return pet_name
+    tags=['example', 'kafka'],
+) as dag:
 
-    @task
-    def get_number_of_treats(num_treats=None):
-        return num_treats
-
-    @task
-    def get_pet_owner_name(your_name=None):
-        return your_name
-
-    produce_treats = ProduceToTopicOperator(
-        task_id="produce_treats",
-        kafka_config_id="kafka_default",
+    # Task to produce a message to the Kafka topic
+    produce_message = ProduceToTopicOperator(
+        task_id='send_message_to_kafka',
+        kafka_conn_id=KAFKA_CONN_ID,
         topic=KAFKA_TOPIC,
-        producer_function=prod_function,
-        producer_function_args=(b"test", b"test"),
-        poll_timeout=10,
+        message=message_payload,
+        value_serializer='json',  # Serializer (e.g., json or simple string)
     )
 
-    consume_treats = ConsumeFromTopicOperator(
-        task_id="consume_treats",
-        kafka_config_id="kafka_default",
-        topics=[KAFKA_TOPIC],
-        apply_function=consume_function,
-        apply_function_kwargs={
-            "name": "{{ ti.xcom_pull(task_ids='get_pet_owner_name')}}"
-        },
-        poll_timeout=20,
-        max_messages=20,
-        max_batch_size=20,
-    )
-
-    [
-        get_your_pet_name(YOUR_PET_NAME),
-        get_number_of_treats(NUMBER_OF_TREATS),
-    ] >> produce_treats
-    get_pet_owner_name(YOUR_NAME) >> consume_treats
-
-    produce_treats >> consume_treats
-
-
-produce_consume_treats()
+    # Set task dependencies
+    produce_message
