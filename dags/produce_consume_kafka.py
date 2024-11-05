bdcbc56c4d32da7b892b24c2fc04c06c3b05f184
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-05 15:17:28 +0530

    Update produce_consume_kafka.py

--- dags/produce_consume_kafka.py
+++ dags/produce_consume_kafka.py
@@ -77,12 +77,12 @@ def produce_consume_treats():
         kafka_config_id="kafka_default",
         topic=KAFKA_TOPIC,
         producer_function=prod_function,
-        provide_context=True,
         producer_function_args=["{{ ti.xcom_pull(task_ids='get_number_of_treats')}}"],
         producer_function_kwargs={
             "pet_name": "{{ ti.xcom_pull(task_ids='get_your_pet_name')}}"
         },
         poll_timeout=10,
+        provide_context=True
     )
 
     consume_treats = ConsumeFromTopicOperator(
@@ -96,6 +96,7 @@ def produce_consume_treats():
         poll_timeout=20,
         max_messages=20,
         max_batch_size=20,
+        provide_context=True
     )
 
     [
