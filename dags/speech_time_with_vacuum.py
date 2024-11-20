cc599e5f7e24e2d31a9f103acb274453823d5c93
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-20 11:29:06 +0530

    Create speech_time_with_vacuum.py

---
+++ dags/speech_time_with_vacuum.py
@@ -0,0 +1,60 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+from datetime import timedelta
+import os
+
+#
+# Define below variables in Airflow UI
+# SPEECHTIME_BRONZE_TABLE_PATH
+# SPEECHTIME_SILVER_TABLE_PATH
+# SPEECHTIME_WINDOW_IN_SECS
+#
+
+BRONZE_TABLE_PATH = Variable.get("SPEECHTIME_BRONZE_TABLE_PATH", 
+                                 default_var="s3a://connect-analytics-platform/dl_engagement_bronze/")
+SILVER_TABLE_PATH = Variable.get("SPEECHTIME_SILVER_TABLE_PATH", 
+                                 default_var="s3a://connect-analytics-platform/dl_engagement_speech_silver/")
+TIME_WINDOW_IN_SECS = Variable.get("SPEECHTIME_WINDOW_IN_SECS", default_var=86400)
+
+run_schedule = Variable.get("SPEECHTIME_SCHEDULE_TIME", default_var=None) # Every 10 mins
+
+# Define default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+   
+# Define the DAG
+with DAG(
+    'speech_time',
+    default_args=default_args,
+    description='A DAG to calculate etl_speech_time',
+    schedule_interval=run_schedule,
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['SpeechTime', 'ETL'],
+) as dag:
+    # Define Operator
+    spark_job = SparkKubernetesOperator(
+        task_id="speech_time",
+        namespace='airflow',
+        application_file='speech_time.yaml',
+        kubernetes_conn_id='spark-cluster-connection',
+        params={
+        "S3_ACCESS_KEY": os.getenv("AWS_S3_ACCESS_KEY"),
+        "S3_SECRET_KEY": os.getenv("AWS_S3_SECRET_KEY"),
+        "S3_END_POINT": os.getenv("AWS_S3_END_POINT"),
+        "BRONZE_TABLE_PATH": BRONZE_TABLE_PATH,
+        "SILVER_TABLE_PATH": SILVER_TABLE_PATH,
+        "TIME_WINDOW_IN_SECS": TIME_WINDOW_IN_SECS
+    }
+)
+
+# Define the task sequence
+spark_job
