1b90f72ec7b884ec5fd6b0c8bc3d8265ac2dac5a
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-20 19:51:33 +0530

    Update speech_time_with_vacuum.py

--- dags/speech_time_with_vacuum.py
+++ dags/speech_time_with_vacuum.py
@@ -1,9 +1,11 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 from datetime import timedelta
 import os
+import logging
 
 #
 # Define below variables in Airflow UI
@@ -11,6 +13,9 @@ import os
 # SPEECHTIME_SILVER_TABLE_PATH
 # SPEECHTIME_WINDOW_IN_SECS
 #
+
+# Define logging
+logger = logging.getLogger(__name__)
 
 BRONZE_TABLE_PATH = Variable.get("SPEECHTIME_BRONZE_TABLE_PATH", 
                                  default_var="s3a://connect-analytics-platform/dl_engagement_bronze/")
@@ -29,7 +34,58 @@ default_args = {
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
-   
+
+# Vacuum table Method
+def vacuum_table():
+    VACUUM_DELTA_TABLE_PATH = SILVER_TABLE_PATH
+    RETENTION_HOURS = 168
+    SEVEN_DAYS_IN_HOURS = 168
+
+    S3_ACCESS_KEY = str(os.getenv("AWS_S3_ACCESS_KEY"))
+    S3_SECRET_KEY = str(os.getenv("AWS_S3_SECRET_KEY"))
+    S3_END_POINT = str(os.getenv("AWS_S3_END_POINT"))
+
+    delta_table_path = VACUUM_DELTA_TABLE_PATH
+    retention_hours = RETENTION_HOURS
+
+    logger.info(f"params {len(S3_ACCESS_KEY)} {len(S3_SECRET_KEY)} {len(S3_END_POINT)} {delta_table_path} {retention_hours}")
+
+    if (len(S3_ACCESS_KEY) > 0) and (len(S3_SECRET_KEY) > 0) and (len(S3_END_POINT) > 0):
+        logger.info("Start vacuuming!!!")
+
+        retention_check = "false" if int(retention_hours) < SEVEN_DAYS_IN_HOURS else "true" # 7 days is default
+
+        spark = SparkSession.builder \
+            .appName("vacuum") \
+            .master("local[*]") \
+            .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.2.0,org.apache.hadoop:hadoop-aws:3.3.1") \
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+            .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+            .config("spark.hadoop.fs.s3a.aws.credentials.provider",
+                    "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+            .config("spark.hadoop.fs.s3a.access.key", S3_ACCESS_KEY) \
+            .config("spark.hadoop.fs.s3a.secret.key", S3_SECRET_KEY) \
+            .config("spark.hadoop.fs.s3a.endpoint", S3_END_POINT) \
+            .config("spark.databricks.delta.retentionDurationCheck.enabled", retention_check) \
+            .getOrCreate()
+
+        try:
+            spark.sql(f'VACUUM delta.`{delta_table_path}` RETAIN {retention_hours} HOURS')
+            # for testing
+            # spark.read.format("delta").load(delta_table_path).printSchema()
+
+        except Exception as e:
+            logger.info(f"An error occurred: {e}")
+        finally:
+            # Stop the Spark session
+            spark.stop()
+            logger.info("Vacuum complete!!!")
+    else:
+        logger.info(f"Invalid params {len(S3_ACCESS_KEY)} {len(S3_SECRET_KEY)} {len(S3_END_POINT)}")
+
+
+
 # Define the DAG
 with DAG(
     'speech_time',
@@ -40,7 +96,7 @@ with DAG(
     catchup=False,
     tags=['SpeechTime', 'ETL'],
 ) as dag:
-    # Define Operator
+    # Speechtime Operator
     spark_job = SparkKubernetesOperator(
         task_id="speech_time",
         namespace='airflow',
@@ -54,7 +110,14 @@ with DAG(
         "SILVER_TABLE_PATH": SILVER_TABLE_PATH,
         "TIME_WINDOW_IN_SECS": TIME_WINDOW_IN_SECS
     }
+
+    # Vacuum Operator
+    vacuum_table = PythonOperator(
+        task_id='vacuum_delta_table',
+        python_callable=vacuum_table,
+        dag=dag
+    )
 )
 
 # Define the task sequence
-spark_job
+vacuum_table >> spark_job
