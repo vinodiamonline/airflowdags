707968b73ddc009aa76639d636b71c23a51dd632
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-23 18:28:09 +0530

    Update speech_time.py

--- dags/speech_time.py
+++ dags/speech_time.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 from datetime import timedelta
