77ddd6a2f275de4e083588196ea7aa4fec2002da
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-26 15:45:33 +0530

    Update speech_time.py

--- dags/speech_time.py
+++ dags/speech_time.py
@@ -53,7 +53,23 @@ with DAG(
     catchup=False,
     tags=['SpeechTime', 'ETL'],
 ) as dag:
-    spark_job = PythonOperator(task_id='speech_time', python_callable=print_hello)
+  # Define Operator
+    spark_job = SparkKubernetesOperator(
+        task_id="speech_time",
+        namespace='airflow',
+        application_file='speech_time.yaml',
+        kubernetes_conn_id='spark-cluster-connection',
+        params={
+          "S3_ACCESS_KEY": os.getenv("AWS_S3_ACCESS_KEY"),
+          "S3_SECRET_KEY": os.getenv("AWS_S3_SECRET_KEY"),
+          "S3_END_POINT": os.getenv("AWS_S3_END_POINT"),
+          "BRONZE_TABLE_PATH": BRONZE_TABLE_PATH,
+          "SILVER_TABLE_PATH": SILVER_TABLE_PATH,
+          "TIME_WINDOW_IN_SECS": TIME_WINDOW_IN_SECS
+        }
+    )
+  
+# spark_job = PythonOperator(task_id='speech_time', python_callable=print_hello)
 
 # Define the task sequence
 spark_job
