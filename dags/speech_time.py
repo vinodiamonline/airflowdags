11aaf0a7ece17912f37f6044d3949070ea70e074
Vinod Kumar <vinodiamonline@gmail.com>
2024-11-26 14:55:23 +0530

    Update speech_time.py

--- dags/speech_time.py
+++ dags/speech_time.py
@@ -1,16 +1,24 @@
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.models import Variable
 from datetime import timedelta
 import os
 
-# run_schedule = Variable.get("SPEECHTIME_SCHEDULE_TIME", default_var=None) # Every 10 mins
+#
+# Define below variables in Airflow UI
+# SPEECHTIME_BRONZE_TABLE_PATH
+# SPEECHTIME_SILVER_TABLE_PATH
+# SPEECHTIME_WINDOW_IN_SECS
+#
 
-print(os.getenv("AWS_S3_ACCESS_KEY"))
-print(os.getenv("AWS_S3_SECRET_KEY"))
-print(os.getenv("AWS_S3_END_POINT"))
+BRONZE_TABLE_PATH = Variable.get("SPEECHTIME_BRONZE_TABLE_PATH", 
+                                 default_var="s3a://connect-analytics-platform/dl_engagement_bronze/")
+SILVER_TABLE_PATH = Variable.get("SPEECHTIME_SILVER_TABLE_PATH", 
+                                 default_var="s3a://connect-analytics-platform/dl_engagement_speech_silver/")
+TIME_WINDOW_IN_SECS = Variable.get("SPEECHTIME_WINDOW_IN_SECS", default_var=86400)
+
+run_schedule = Variable.get("SPEECHTIME_SCHEDULE_TIME", default_var=None) # Every 10 mins
 
 # Define default arguments
 default_args = {
@@ -21,22 +29,32 @@ default_args = {
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
-
-def print_hello():
-    print('Hello world from first Airflow DAG!')
-
-dag = DAG('speech_time', description='speech time',
-          schedule_interval=None,
-          start_date=days_ago(1), 
-          catchup=False)
-
-# spark_job = PythonOperator(task_id='speech_time', python_callable=print_hello, dag=dag)
-spark_job = SparkKubernetesOperator(
+   
+# Define the DAG
+with DAG(
+    'speech_time',
+    default_args=default_args,
+    description='A DAG to calculate etl_speech_time',
+    schedule_interval=None,
+    start_date=days_ago(1),
+    catchup=False,
+    tags=['SpeechTime', 'ETL'],
+) as dag:
+    # Define Operator
+    spark_job = SparkKubernetesOperator(
         task_id="speech_time",
         namespace='airflow',
         application_file='speech_time.yaml',
-        kubernetes_conn_id='spark-local',
-        dag=dag)
+        kubernetes_conn_id='spark-cluster-connection',
+        params={
+        "S3_ACCESS_KEY": os.getenv("AWS_S3_ACCESS_KEY"),
+        "S3_SECRET_KEY": os.getenv("AWS_S3_SECRET_KEY"),
+        "S3_END_POINT": os.getenv("AWS_S3_END_POINT"),
+        "BRONZE_TABLE_PATH": BRONZE_TABLE_PATH,
+        "SILVER_TABLE_PATH": SILVER_TABLE_PATH,
+        "TIME_WINDOW_IN_SECS": TIME_WINDOW_IN_SECS
+    }
+)
 
 # Define the task sequence
 spark_job
